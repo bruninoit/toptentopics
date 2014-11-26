c9e7f0e4ebcca71c298ade5a88c938d560e3f0bb
bruninoit <brunino98@hotmail.it>
2014-11-26 13:52:54 +0100

    b2

--- language/en/common.php
+++ language/en/common.php
@@ -13,4 +13,12 @@ $lang = array_merge($lang, array(
 	'LAST_TOPICS'			=> 'Last Topics',
     'LAST_POSTS'			=> 'Last Posts',
 	'TOP_TOPICS'			=> 'Top Topics',
+	'ACP_TTT_TITLE'			=> 'General Settings',
+	'ACP_TTT_CATEGORY'		=> 'Top Ten Topics',
+	'ACP_POSITION'		=> 'Index position of box',
+	'ACP_GUEST'		=> 'Show box to guest',
+	'ACP_NUMBER'		=> 'Number of topic/post extracted',
+	'BEFORE_FORUMLIST'		=> 'Before forum list',
+	'AFTER_FORUMLIST'		=> 'after forum list',
+	'ACP_TTT_SAVED'			=> 'Settings of Top Ten Topics saved',
 ));
