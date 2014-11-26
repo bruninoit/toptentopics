a2fd326f34b55392696be52df4f90a1efca15d82
bruninoit <brunino98@hotmail.it>
2014-11-26 13:58:34 +0100

    test

--- migrations/release_0_1_0.php
+++ migrations/release_0_1_0.php
@@ -19,7 +19,7 @@ class release_0_1_0 extends \phpbb\db\migration\migration
 			array('module.add', array(
 				'acp',
 				'ACP_CAT_DOT_MODS',
-				'ACP_TTT_CATEGORY'
+				'ACP_TTT_CATEGORY',
 			)),
 			array('module.add', array(
 				'acp',
