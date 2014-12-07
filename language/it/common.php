0fafea9c9f0d6fb40ca745061dc6142319d42129
bruninoit <brunino98@hotmail.it>
2014-12-07 13:35:55 +0100

    aggiornte opzioni pca

--- language/it/common.php
+++ language/it/common.php
@@ -18,7 +18,8 @@ $lang = array_merge($lang, array(
 	'ACP_POSITION'		=> 'Scegli la posizione del box nell&apos;indice',
 	'ACP_GUEST'		=> 'Vuoi che sia mostrata agli utenti non registrati?',
 	'ACP_NUMBER'		=> 'Numero di topic e post estratti',
-	'BEFORE_FORUMLIST'		=> 'Prima della lista dei forum',
+	'BEFORE_FORUMLIST'		=> 'Prima della lista dei forum ma dopo il link "segna come gi&agrave; letti"',
 	'AFTER_FORUMLIST'		=> 'Dopo la lista dei forum',
+	'L_AFTER_MARKFORUM'		=> 'Prima della lista dei forum e del link "segna come gi&agrave; letti"',
 	'ACP_TTT_SAVED'			=> 'Impostazioni della Top Ten Topics aggiornate'
 ));
