64a202c257758305c056b0ba2ee150954f0d3f8a
bruninoit <brunino98@hotmail.it>
2014-12-12 19:42:56 +0100

    commenti iniziali

--- migrations/release_0_1_0.php
+++ migrations/release_0_1_0.php
@@ -1,4 +1,11 @@
 <?php
+/** 
+* 
+* @package StaffIt - Top Ten Topics 
+* @copyright (c) 2014 brunino
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
 namespace staffit\toptentopics\migrations;
 class release_0_1_0 extends \phpbb\db\migration\migration
 {
