71871510163f363ee2f3039b7a5b97efd7a8ff21
bruninoit <brunino98@hotmail.it>
2014-12-12 19:39:01 +0100

    commenti iniziali

--- acp/main_module.php
+++ acp/main_module.php
@@ -1,4 +1,11 @@
 <?php
+/** 
+* 
+* @package StaffIt - Top Ten Topics 
+* @copyright (c) 2014 phpBB Group 
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
 namespace staffit\toptentopics\acp;
 class main_module
 {
