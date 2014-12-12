efdda20f32478c2c99b08473726ab8c78713fd9a
bruninoit <brunino98@hotmail.it>
2014-12-12 19:42:02 +0100

    commenti iniziali

--- language/it/common.php
+++ language/it/common.php
@@ -1,4 +1,11 @@
 <?php
+/** 
+* 
+* @package StaffIt - Top Ten Topics 
+* @copyright (c) 2014 brunino
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
 if (!defined('IN_PHPBB'))
 {
 	exit;
