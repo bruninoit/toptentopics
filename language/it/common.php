01a98f69eff221ffb4ee3febb3f7f420c65b843e
bruninoit <brunino98@hotmail.it>
2014-12-13 19:12:26 +0100

    important

--- language/it/common.php
+++ language/it/common.php
@@ -26,6 +26,7 @@ $lang = array_merge($lang, array(
 	'ACP_GUEST'		=> 'Vuoi che sia mostrata agli utenti non registrati?',
 	'ACP_NUMBER'		=> 'Numero di topic e post estratti',
 	'ACP_DATA'		=> 'Periodo predefinito dei topic pi&ugrave; visti',
+	'ACP_IMPORTANT'		=> 'Scegli quali tipi di topic visualizzare'
 	'BEFORE_FORUMLIST'		=> 'Prima della lista dei forum e del link "segna come gi&agrave; letti"',
 	'AFTER_FORUMLIST'		=> 'Dopo la lista dei forum e del box di login',
 	'AFTER_MARKFORUM'		=> 'Prima della lista dei forum, ma dopo link "segna come gi&agrave; letti"',
@@ -37,5 +38,9 @@ $lang = array_merge($lang, array(
 	'ONEMONTH' => 'ultimo mese',
 	'THREEMONTH' => 'ultimi 3 mesi',
 	'ALL_TOPICS' => 'Tutti',
-	'TTT_BASED' => 'basata sulla MOD di <a href="http://giannidose.altervista.org">Micogian</a>'
+	'TTT_BASED' => 'basata sulla MOD di <a href="http://giannidose.altervista.org">Micogian</a>',
+	'TNORMAL' => 'Solo topic normali',
+	'TIMPORTANT' => 'Topic normali e importanti',
+	'TANNUNCE' => 'Topic normali, importanti e annunci',
+	'TGLOBAL' => 'Topic normali, importanti, annunci e annunci globali',
 ));
