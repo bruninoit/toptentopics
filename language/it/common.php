1316aa1aa2368135e84c0bca45b5ef9386225bc6
Raphaël M. <Galixte@users.noreply.github.com>
2015-09-12 17:23:47 +0200

    Update languages EN & IT

--- language/it/common.php
+++ language/it/common.php
@@ -1,11 +1,16 @@
 <?php
-/** 
-* 
-* @package StaffIt - Top Ten Topics 
-* @copyright (c) 2014 brunino
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
-* 
-*/ 
+/**
+*
+* Top Ten Topics extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 brunino <http://www.brunino.altervista.org>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -16,27 +21,43 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
 	'LAST_TOPICS'			=> 'Ultimi Topic',
     'LAST_POSTS'			=> 'Ultimi Post',
 	'TOP_TOPICS'			=> 'Argomenti più visti',
 	'ACP_TTT_TITLE'			=> 'Impostazioni generali',
 	'ACP_TTT_CATEGORY'		=> 'Top Ten Topics',
-	'ACP_POSITION'		=> 'Scegli la posizione del box nell\'indice',
-	'ACP_POSITION_DESC'	=> 'N.B. Per usare la posizione "Dopo la lista dei forum, ma prima del box di login" devi avere lo/gli stile/i in uso aggiornato/i almeno a phpBB 3.1.1',
+	'ACP_POSITION'		=> 'Scegli la posizione del box nell’indice',
+	'ACP_POSITION_DESC'	=> 'N.B. Per usare la posizione "Dopo la lista dei forum, ma prima del box di login" devi avere lo/gli stile/i in uso aggiornato/i almeno a phpBB 3.1.1.',
 	'ACP_GUEST'		=> 'Vuoi che sia mostrata agli utenti non registrati?',
 	'ACP_NUMBER'		=> 'Numero di topic e post estratti',
-	'ACP_NUMBER_DESC'	=> 'Se inserisci un numero di topic maggiore di quanti lutente può visualizzare, la pagina genererà un errore.',
+	'ACP_NUMBER_DESC'	=> 'Se inserisci un numero di topic maggiore di quanti lutente può visualizzare, la pagina genererà un errore.',
 	'ACP_DATA'		=> 'Periodo predefinito dei topic più visti',
 	'ACP_IMPORTANT'		=> 'Scegli quali tipi di topic visualizzare',
 	'ACP_FORUM'		=> 'Sezioni da escludere',
-	'ACP_FORUM_DESC'	=> 'Devi inserire gli ID delle sezioni da escludere, separati solo da virgole senza spazi: ad esempio 1,2,3<br>Se non vuoi escludere nessuna sezione lascia questo campo vuoto. Considera che lestensione non mostra agli utenti i topic postati nei forum a cui loro non hanno accesso.',
+	'ACP_FORUM_DESC'	=> 'Devi inserire gli ID delle sezioni da escludere, separati solo da virgole senza spazi: ad esempio 1,2,3.<br>Se non vuoi escludere nessuna sezione lascia questo campo vuoto. Considera che lestensione non mostra agli utenti i topic postati nei forum a cui loro non hanno accesso.',
 	'BEFORE_FORUMLIST'		=> 'Prima della lista dei forum e del link "segna come gi&agrave; letti"',
 	'AFTER_FORUMLIST'		=> 'Dopo la lista dei forum e del box di login',
 	'AFTER_MARKFORUM'		=> 'Prima della lista dei forum, ma dopo link "segna come gi&agrave; letti"',
 	'BEFORE_LOGIN' 			=> 'Dopo la lista dei forum, ma prima del box di login',
 	'ALL_FOOTER'			=> 'In tutte le pagine, nel footer',
-	'ACP_TTT_SAVED'			=> 'Impostazioni della Top Ten Topics aggiornate',
+	'ACP_TTT_SAVED'			=> 'Impostazioni della Top Ten Topics aggiornate.',
 	'SIXMONTH' => 'ultimi 6 mesi',
 	'TWELVEMONTH' => 'ultimi 12 mesi',
 	'ONEMONTH' => 'ultimo mese',
