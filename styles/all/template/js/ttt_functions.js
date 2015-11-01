af7abdfc601397a550614c5dd2af37be63fdf3f0
Xiradorn <xiradorn@gmail.com>
2015-11-01 19:19:10 +0100

    New Feature: Filter for column

--- styles/all/template/js/ttt_functions.js
+++ styles/all/template/js/ttt_functions.js
@@ -16,14 +16,40 @@
 	 	domScope: function() {
 	 		this.$selectorOption = $('.xttt_select');
 	 		this.$selectorForm = $('#xttt_form');
+	 		/* filters targeting */
+	 		this.$dt = $('dt.xttt-one_third');
+			this.$dd = $('dd.xttt-one_third');
+			/* filters buttons */
+			this.$filterBtn = $('.xttt-filter_btn');
+			this.$most_t = $('.xtttf_most_view');
+			this.$last_t = $('.xtttf_last_topic');
+			this.$last_p = $('.xtttf_last_post');
 	 	},
 
 	 	render: function() {
 	 		this.$selectorOption.on('change', this.submitForm);
+	 		/* filters */
+	 		this.$filterBtn.on('click', this.filterTTT);
 	 	},
 
 	 	submitForm: function() {
 	 		this.form.submit();
+	 	},
+
+	 	filterTTT: function() {
+	 		// under building
+	 	},
+
+	 	responsive: function(element, hide) {
+	 		if (!hide) {
+	 			element.addClass('responsive-show');
+				element.removeClass('responsive-hide');
+				element.attr('style', 'display: block !important;');
+			} else {
+				element.addClass('responsive-hide');
+				element.removeClass('responsive-show');
+				element.attr('style', 'display: none !important;');
+			}
 	 	}
 	 }
 
