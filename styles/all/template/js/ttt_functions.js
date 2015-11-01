5a434e294583710c94b14fc3f37458a777fd22c9
Xiradorn <xiradorn@gmail.com>
2015-11-01 20:47:12 +0100

    New Feature: Filter for column

--- styles/all/template/js/ttt_functions.js
+++ styles/all/template/js/ttt_functions.js
@@ -19,28 +19,44 @@
 	 		/* filters targeting */
 	 		this.$dt = $('dt.xttt-one_third');
 			this.$dd = $('dd.xttt-one_third');
+			this.$ddl = $('dd.xttt-one_third_last');
 			/* filters buttons */
 			this.$filterBtn = $('.xttt-filter_btn');
-			this.$most_t = $('.xtttf_most_view');
-			this.$last_t = $('.xtttf_last_topic');
-			this.$last_p = $('.xtttf_last_post');
 	 	},
 
 	 	render: function() {
 	 		this.$selectorOption.on('change', this.submitForm);
 	 		/* filters */
-	 		this.$filterBtn.on('click', this.filterTTT);
+	 		this.$filterBtn.on('click', this.filterTTT.bind(this));
 	 	},
 
 	 	submitForm: function() {
 	 		this.form.submit();
 	 	},
 
-	 	filterTTT: function() {
-	 		// under building
+	 	filterTTT: function(e) {
+	 		e.preventDefault();
+	 		var trg;
+
+	 		trg = $(e.target);
+	 		console.log(this.$dt);
+
+	 		if(trg.hasClass('xtttf_most_view')) {
+	 			this.hideCall(false, true, true);
+	 		} else if (trg.hasClass('xtttf_last_topic')) {
+	 			this.hideCall(true, false, true);
+	 		} else if (trg.hasClass('xtttf_last_post')) {
+	 			this.hideCall(true, true, false);
+	 		}
 	 	},
 
-	 	responsive: function(element, hide) {
+	 	hideCall: function(dt_hide, dd_hide, ddl_hide) {
+	 		this.hiderTarget(this.$dt, dt_hide);
+ 			this.hiderTarget(this.$dd, dd_hide);
+ 			this.hiderTarget(this.$ddl, ddl_hide);
+	 	},
+
+	 	hiderTarget: function(element, hide) {
 	 		if (!hide) {
 	 			element.addClass('responsive-show');
 				element.removeClass('responsive-hide');
